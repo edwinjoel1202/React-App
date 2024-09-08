b482518447f48d1c6f0892b20e948fd343c95ef4
edwinjoel1202 <edwinjoel1204@gmail.com>
2024-09-08 19:50:17 +0530

    Food.jsx added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import "./components/Header"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import Food from "./components/Food"
 
 
 
@@ -9,6 +9,7 @@ function App() {
   return(
     <>
       <Header/>
+      <Food/>
       <Footer/>
     </>
   )
---
+++ src/components/Food.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Food() {
+  const food1 = "Briyani";
+  const food2 = "Paneer Butter Masala";
+
+  return (
+    <div>
+    <h2>Foods</h2>
+      <ul>
+        <li>{food1.toUpperCase()}</li>
+        <li>{food2.toUpperCase()}</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Food;
